e21ba43bf1002387aa4631d70d853b2955b9f934
sirtoxic <leerussell1991@gmail.com>
2016-06-15 02:28:01 +1000

    made changes to handlebar information

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Conquest of Nerath Dice Roller' });
 });
 
 module.exports = router;
